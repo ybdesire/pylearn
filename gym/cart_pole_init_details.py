e44ac7b58adc795a5e9a4bedc27e5476c3b3d942
ybdesire <ybdesire@gmail.com>
2018-07-24 21:56:48 +0800

    CartPole env init values

---
+++ gym/cart_pole_init_details.py
@@ -0,0 +1,10 @@
+﻿import gym
+
+env = gym.make('CartPole-v0')   # 定义使用 gym 库中的那一个环境
+env = env.unwrapped # 不做这个会有很多限制
+
+print(env.action_space, env.action_space.n, ) # 查看这个环境中可用的 action 有多少个
+print(env.observation_space)    # 查看这个环境中可用的 state 的 observation 有多少个
+print(env.observation_space.high)   # 查看 observation 最高取值
+print(env.observation_space.low)    # 查看 observation 最低取值
+print(env.reward_range)
