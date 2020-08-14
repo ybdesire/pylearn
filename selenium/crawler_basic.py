80181ec222e1e7eadc8064a538235b39a65a68e2
ybdesire <ybdesire@gmail.com>
2020-08-14 23:14:36 +0800

    Add files via upload

---
+++ selenium/crawler_basic.py
@@ -0,0 +1,20 @@
+# reference: https://www.cnblogs.com/lweiser/p/11045023.html
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait 
+
+browser=webdriver.Chrome()
+
+browser.implicitly_wait(10)
+
+browser.get('http://data.aiweibang.com/rank/account?t=1')# open the url
+
+
+divs = browser.find_elements_by_xpath('//div[@class="numberPublic"]')# find the div with special class name
+for d in divs:
+    print(d.text)
+
+browser.close()
